a03540cf0e8868ac58d4fdc02be185db763b7e25
Craig Foster <craig.fozzy1@gmail.com>
2018-12-02 10:12:20 +0000

    Day2

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,14 @@
 object AoC {
   def main(args: Array[String]): Unit = {
-    println("Day 1 Answer 1: ")
-    println(Problems.day1_1("InputFiles/Day1_1.txt",0))
-    println("Day 1 Answer 2: ")
-    println(Problems.day1_2("InputFiles/Day1_1.txt"))
+    /*
+     println("Day 1 Answer 1: ")
+     println(Problems.day1_1("InputFiles/Day1_1.txt",0))
+     println("Day 1 Answer 2: ")
+     println(Problems.day1_2("InputFiles/Day1_1.txt"))*/
+
+   // println("Day 2 Answer 1: ")
+    //println(Problems.day2_1("InputFiles/Day2_1.txt"))
+    println("Day 2 Answer 2: ")
+    println(Problems.day2_2("InputFiles/Day2_1.txt"))
   }
 }
--- src/main/scala/Problems.scala
+++ src/main/scala/Problems.scala
@@ -36,5 +36,49 @@ object Problems {
     return 0
   }
 
+  def day2_1(inputFile: String): Int = {
+    var twoLetter = 0
+    var threeLetter = 0
+
+    var letters:scala.collection.mutable.Map[Char,Int] = scala.collection.mutable.Map()
+    val f = new BufferedReader(new FileReader(inputFile))
+    while (f.ready()) {
+      val line = f.readLine()
+      for(x:Char <- line){
+        letters += (x -> (letters.getOrElse(x,0) + 1))
+      }
+      //check for exactly 2 or 3 values
+      if(letters.values.exists(_ == 2)){
+        twoLetter += 1
+      }
+      if(letters.values.exists(_ == 3)){
+        threeLetter += 1
+      }
+      //clear the letters map
+      letters.clear()
+    }
+    return twoLetter * threeLetter
+  }
+
+  def day2_2(inputFile: String): String = {
+    var stringSet: scala.collection.mutable.Set[String] = scala.collection.mutable.Set()
+
+    val f = new BufferedReader(new FileReader(inputFile))
+    while (f.ready()) {
+      val line = f.readLine()
+        stringSet.add(line)
+      }
+    while(true){
+        stringSet.tail.foreach(u => if((u zip stringSet.head).count{case (x,y) => x != y} == 1 ){
+        //one char doesn't match
+         //return u.replace(u.diff(stringSet.head),"") // REPLACING ALL OF THIS LETTER :(
+          var x = (u zip stringSet.head).indexWhere{case (x,y) => x != y}
+          return u.substring(0,x) + u.substring(x+1,u.length)
+        println("test")
+      })
+      stringSet.remove(stringSet.head)
+      }
+    return ""
+  }
 
 }
